52b5d650039b1206f5c4ce02c020f67172a6b814
noufalktt <32330005+noufalktt@users.noreply.github.com>
2017-10-16 10:51:09 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 
 sudo apt-get update
@@ -10,3 +11,6 @@ sudo apt-get -y install docker-ce
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+mkdir smartshipsetup
+cd smartshipsetup
