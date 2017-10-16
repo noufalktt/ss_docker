45fb4f18b69501ea7c74976a22e5f686daefc7af
noufalktt <32330005+noufalktt@users.noreply.github.com>
2017-10-16 09:18:15 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-ls
-pwd
+
+sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
